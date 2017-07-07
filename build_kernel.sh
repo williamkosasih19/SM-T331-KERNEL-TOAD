24521f95bdb8b55bbb80c29ec16c651d92d31550
williamkosasih19 <williamkosasih19@gmail.com>
2017-07-07 18:40:05 +0700

    update

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export ARCH=arm
-export CROSS_COMPILE=/opt/toolchains/arm-eabi-4.7/bin/arm-eabi-
+export CROSS_COMPILE=/home/williamkosasih/arm-eab4.7/bin/arm-eabi-
 mkdir output
 
 make -C $(pwd) O=output VARIANT_DEFCONFIG=msm8226-sec_millet3g_defconfig msm8226-sec_defconfig SELINUX_DEFCONFIG=selinux_defconfig
