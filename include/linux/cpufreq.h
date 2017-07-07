baec6e8885dc9660009e4aaccb6076af161efe85
williamkosasih19 <williamkosasih19@gmail.com>
2017-07-07 21:47:09 +0700

    bugfix

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -460,7 +460,6 @@ extern struct cpufreq_governor cpufreq_gov_conservative;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVE)
 extern struct cpufreq_governor cpufreq_gov_interactive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_interactive)
-#endif
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTASS)
 extern struct cpufreq_governor cpufreq_gov_smartass;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_smartass)
